3c0eb0ce503a410044e2131cd26f6e5a435c976b
David R. Piegdon <dgit@piegdon.de>
2019-08-17 21:33:26 +0000

    minimal example to reproduce bug described in https://github.com/DurandA/verilog-buildingblocks/issues/1
    (for ice40)

--- top.v
+++ top.v
@@ -12,59 +12,11 @@ module top(input wire CLK,
 	input wire RX,
 	output wire TX);
 
-	wire [7:0] uart_rxByte;
-	wire uart_received;
-
-	wire reset_in;
-	assign reset_in = uart_received && (uart_rxByte == 8'h72);
-
-	wire rst;
-	synchronous_reset_timer resetter(CLK, rst, reset_in);
-
-	wire [15:0] lfsr;
-	wire metastable;
-	wire bit_ready;
-	wire word_ready;
-	randomized_lfsr randomized_lfsr(CLK, rst, bit_ready, word_ready, lfsr, metastable);
-
-	wire is_transmitting;
-	wire dataReady;
-
-	uart #(.CLOCKFRQ(12000000), .BAUDRATE(3000000) ) uart(
-		.clk(CLK),
-		.rst(rst),
-		.rx(RX),
-		.tx(TX),
-		.transmit(!is_transmitting & word_ready),
-		.tx_byte(lfsr[7:0]),
-		.received(uart_received),
-		.rx_byte(uart_rxByte),
-		.is_receiving(),
-		.is_transmitting(is_transmitting),
-		.recv_error()
-	);
-
-	wire ringosci_insane_out;
-	ringoscillator #(.DELAY_LUTS(0))
-		ringosci_insane(ringosci_insane_out);
-
 	wire ringosci_fast_out;
 	ringoscillator #(.DELAY_LUTS(1))
 		ringosci_fast(ringosci_fast_out);
 
-	wire ringosci_slow_out;
-	ringoscillator #(.DELAY_LUTS(20))
-		ringosci_slow(ringosci_slow_out);
-
-	assign J1_10 = metastable;
-
-	assign J1_9 = lfsr[0];
-	assign J1_8 = bit_ready;
-	assign J1_7 = word_ready;
-	assign J1_6 = 0;
-	assign J1_5 = ringosci_insane_out;
 	assign J1_4 = ringosci_fast_out;
-	assign J1_3 = ringosci_slow_out;
 
 endmodule
 
